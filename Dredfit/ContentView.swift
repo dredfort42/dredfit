fbe08d3cfad901702c26addc08b7b269a52837fb
Dmitry Novikov <dredfort.42@gmail.com>
2026-07-16 12:52:52 +0100

    Initial Commit

---
+++ Dredfit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dredfit
+//
+//  Created by Dmitry Novikov on 16/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dredfit/DredfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DredfitApp.swift
+//  Dredfit
+//
+//  Created by Dmitry Novikov on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct DredfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
